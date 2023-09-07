3874f04f198c2fc491cc7d4d76f47d20fca990cc
Aniket <ghoshaniket1000@gmail.com>
2023-09-07 14:32:38 +0530

    fixed error and implimented function to update the counter

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
- // document.getElementById("count-el").innerHTML = 5;
-// let count = 0
-// function increment() {
-//     console.log("button was clicked")
-// }
-function countdown() {
-    console.log(5)
-    console.log(4)
-    console.log(3)
-    console.log(2)
-    console.log(1)
+let ele = document.getElementById("count-el")
+let count = 0
+function increment() {
+    count  = count + 1
+    ele.innerText = count
 }
-// setting up the race
-
-countdown()
-
-// Go
-// players are running the race
-// race is finished!
-
-//race again
-
-countdown()
