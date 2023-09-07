cd396368b4e5cca8224a3f1536cdb44e538cf252
Aniket <ghoshaniket1000@gmail.com>
2023-09-07 11:46:39 +0530

    added styling and increment button

---
+++ index.js
@@ -0,0 +1,10 @@
+ // document.getElementById("count-el").innerHTML = 5;
+// let count = 0
+
+// console.log(count)
+
+let myage = 18
+let humanDogRatio = 7
+myDogAge = myage * humanDogRatio
+document.getElementById("count-el").innerHTML = myDogAge
+console.log(myDogAge)
