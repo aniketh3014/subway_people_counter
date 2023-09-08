cd84a221a1947f14ea0cea414412fea02b18777b
Aniket <ghoshaniket1000@gmail.com>
2023-09-08 22:30:40 +0530

    added save button that keeps track of previous entries

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let ele = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0
 function increment() {
-    count  = count + 1
+    count += 1
     ele.innerText = count
 }
 function save() {
+    let countStr = count + " - "
+    saveEl.innerText += countStr   
     console.log(count)
 }
